205135de0c14700ab3b3fbbad38d3999b1ad4629
Thanayut <614259018@webmail.npru.ac.th>
2020-09-29 17:04:08 +0700

    up

---
+++ application/controllers/History_section.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class History_section extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('form_section');
+	}
+	public function reg_section(){
+		$data = array(
+			's_id' =>$this->input->post("s_id"),
+			'name' =>$this->input->post("name"),
+			'lname' =>$this->input->post("lname"),
+			'date' =>$this->input->post("date"),
+			'mont' =>$this->input->post("mont"),
+			'years' =>$this->input->post("years"),
+			'address' =>$this->input->post("address"),
+			'Tel' =>$this->input->post("Tel"),
+			'major' =>$this->input->post("major"),
+			'syears' =>$this->input->post("syears"),
+		);
+		$get['a'] = $data;
+		$this->load->view('view_section',$get);
+	}
+
+}
---
+++ application/views/view_section.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+	
+</head>
+<body>
+
+	<?php
+		echo $a['s_id'];
+		echo $a['name'];
+		echo $a['lname'];
+		echo $a['date'];
+		echo $a['mont'];
+		echo $a['years'];
+		echo $a['address'];
+		echo $a['Tel'];
+		echo $a['major'];
+		echo $a['syears'];
+	?>
+
+</body>
+</html>
